6784f87f265b651c03fb59ad4533a6a2d1122b26
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2017-12-23 15:00:40 +0530

    GTP

---
+++ role/_GoToPoint.py
@@ -0,0 +1,68 @@
+from kubs import kubs, cmd_node
+from velocity.run import *
+from velocity.run_w import *
+import rospy,sys
+from krssg_ssl_msgs.msg import point_2d
+from krssg_ssl_msgs.msg import BeliefState
+from krssg_ssl_msgs.msg import gr_Commands
+from krssg_ssl_msgs.msg import gr_Robot_Command
+from utils.geometry import Vector2D
+from utils.config import *
+from utils.math_functions import *
+
+kub = None
+start_time = None
+GOAL_POINT = None
+FLAG_move = True
+FLAG_turn = True
+rotate = 0
+init_angle = 0
+
+FIRST_CALL = True
+
+
+def init(_kub,target,theta):
+    global kub,GOAL_POINT,rotate
+    kub = _kub
+    GOAL_POINT = point_2d()
+    rotate = theta
+    GOAL_POINT.x = target.x
+    GOAL_POINT.y = target.y
+
+
+def reset():
+    global start_time
+    start_time = rospy.Time.now()
+    start_time = 1.0*start_time.secs + 1.0*start_time.nsecs/pow(10,9)
+    
+def execute(startTime):
+    global GOAL_POINT, start_time,FIRST_CALL
+    # print 
+    if FIRST_CALL:
+        start_time = startTime
+        FIRST_CALL = False
+
+    t = rospy.Time.now()
+    t = t.secs + 1.0*t.nsecs/pow(10,9)
+
+    [vx, vy, vw, REPLANNED] = Get_Vel(start_time, t, kub.kubs_id, GOAL_POINT, kub.state.homePos, kub.state.awayPos)
+    vw = Get_Omega(kub.kubs_id,rotate,kub.state.homePos)
+    
+    if not vw:
+        print "Didn't receive Omega"
+        vw = 0
+
+    if(REPLANNED):
+        reset()
+    kub.move(vx, vy)
+    kub.turn(vw)
+
+    # print radian_2_deg(kub.state.homePos[kub.kubs_id].theta),radian_2_deg(rotate),vw
+
+    if abs(radian_2_deg(kub.state.homePos[kub.kubs_id].theta)-radian_2_deg(rotate))<ROTATION_FACTOR:
+        kub.turn(0)
+
+    if dist(kub.state.homePos[kub.kubs_id], GOAL_POINT) < DISTANCE_THRESH :
+        kub.move(0,0)
+    
+    kub.execute()
---
+++ utils/math_functions.py
@@ -0,0 +1,68 @@
+import math
+from ctypes import *
+from geometry import Vector2D
+from config import *
+def return_line_in_general_form(slope=None,intercept=None,point1=None,point2=None,r=None,theta=None):
+  pass
+
+def return_circle_in_general_form(center=None,radius=None,point1=None,point2=None,point3=None):
+  pass
+
+def line_circle_intersection(line,cirlce):
+  pass
+
+def point_in_a_triangle(point,triangle):
+  pass
+
+def line_intersection(line1,line2):
+  pass
+
+def line_ellipse_intersection(line,ellipse):
+  pass
+
+def normalize_angle(angle):
+  if angle > math.pi :
+    return angle - 2 * math.pi
+  elif angle <= -math.pi :
+    return angle + 2 * math.pi
+  else :
+    return angle
+
+def getPointBehindTheBall(point ,theta):
+  x = point.x -(1.2 * BOT_RADIUS) *(math.cos(theta))
+  y = point.y -(1.2 * BOT_RADIUS) *(math.sin(theta))
+  return Vector2D(int(x), int(y))
+
+def deg_2_radian(theta):
+  return theta * math.pi / 180.0
+
+def radian_2_deg(theta):
+  return theta*180.0 /math.pi 
+
+def dist(point1,point2):
+  return math.sqrt((point1.x-point2.x)**2+(point1.y-point2.y)**2)
+
+def angle_diff(point1,point2):
+  return math.atan2(point2.y-point1.y,point2.x-point1.x)
+
+
+def angle_at_vextex(P1,P2,P3):
+    a     = P2.dist(P3)
+    b     = P1.dist(P3)
+    c     = P1.dist(P2)
+    value = (b**2 + c**2 - a**2)/(2*b*c)
+    theta = math.acos(value)
+    return math.atan2(math.tan(theta),1.0)
+
+def area_of_triangle(p1,p2,p3):
+    return math.abs(p1.x*(p2.y-p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y-p2.y))/2.0
+
+def point_in_triangle(t, P):
+    P1, P2, P3 = t[0], t[1], t[2]
+
+    a  = area_of_triangle(P1,P2,P3)
+    a1 = area_of_triangle(P,P2,P3)
+    a2 = area_of_triangle(P1,P,P3)
+    a3 = area_of_triangle(P1,P2,P)
+
+    return a == a1+a2+a3
